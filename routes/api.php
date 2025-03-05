a6d37b2477e6b96e59e018dcaf52a4f8b53fc3d9
usepgnwan <usepgnwan@gmail.com>
2025-03-05 10:23:22 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('tes', function(){
-echo "jajaja";
-});
-
+ 
 Route::post('/github-webhook', function () {
+    if (!file_exists('/home/u504186664/domains/rifayaeducation.com/public_html/deploy.sh')) {
+        Log::error("deploy.sh does not exist at the expected path.");
+        return response()->json(['message' => 'deploy.sh file not found'], 500);
+    }
+
     Log::info('GitHub Webhook Received: ');
     
     $output = shell_exec('whoami 2>&1'); // Check user running the script
@@ -20,7 +22,7 @@ Route::post('/github-webhook', function () {
     $output = shell_exec('ls -l ./../deploy.sh 2>&1'); // Check script permissions
     Log::info("File permissions: $output");
 
-    $output = shell_exec('./../deploy.sh'); // Capture errors
+    $output = shell_exec('/home/u504186664/domains/rifayaeducation.com/public_html/deploy.sh 2>&1'); // Capture errors
     Log::info("Shell Output: $output");
 
     if (is_null($output) || trim($output) === "") { 
