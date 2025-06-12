a1e3421075042358627ba9ba378d28776557cfb2
usepgnwan <usepgnwan@gmail.com>
2025-06-12 22:50:42 +0700

    update

--- resources/views/livewire/front/nilai-siswa.blade.php
+++ resources/views/livewire/front/nilai-siswa.blade.php
@@ -1,13 +1,13 @@
 <section class="max-h-screen h-screen relative"> 
     <div class="bg-[#FABE0E]  fixed w-full h-1/2"></div>
-    <div class="w-full mt-3 max-sm:p-4 sm:h-screen space-y-4 sm:py-2 absolute z-20 pb-52">
+    <div class="w-full mt-3 max-sm:p-4 sm:h-screen space-y-4 sm:py-2 absolute z-20">
         <div class="max-sm:flex hidden  items-center justify-center text-white text-xl">
                <a href="{{ route('home') }}" class="flex space-x-2 items-center "> 
             <img src="{{ asset('asset/img/home/logo-rifaya.png') }}" alt="" class="w-5">
             <span> Source By Rifaya Education</span>
         </a>
         </div>
-        <div class="bg-white  sm:my-auto  sm:w-9/12 mx-auto   sm:py-3  pb-52 w-full p-5 rounded-xl shadow-md border-t-4 border-t-blue-700 dark:border dark:border-slate-800 mb-5 dark:border-t-4"> 
+        <div class="bg-white  sm:my-auto  sm:w-9/12 mx-auto   sm:py-3  w-full p-5 rounded-xl shadow-md border-t-4 border-t-blue-700 dark:border dark:border-slate-800 mb-5 dark:border-t-4"> 
             <div class="text-justify"> 
                 <h2 class="text-2xl font-extrabold mb-4">Data Siswa</h2>
                  <div class="flex space-x-2 text-lg">
@@ -17,7 +17,7 @@
             </div>
         </div>
         @forelse ($myNilai as $k=> $v)
-        <div class="min-h-96  bg-white  sm:my-auto  sm:w-9/12 mx-auto   sm:py-3  pb-52 w-full p-5 rounded-xl shadow-md border-t-4 border-t-blue-700 dark:border dark:border-slate-800 mb-5 dark:border-t-4 ">
+        <div class="min-h-96  bg-white  sm:my-auto  sm:w-9/12 mx-auto   sm:py-3  w-full p-5 rounded-xl shadow-md border-t-4 border-t-blue-700 dark:border dark:border-slate-800 mb-5 dark:border-t-4 ">
             <div class="text-justify"> 
                 <h2 class="text-2xl font-extrabold mb-4">{{ $k }}</h2> 
                 <div class="overflow-x-auto">
@@ -71,7 +71,7 @@
             </div> 
         </div>
         @empty
-        <div class="min-h-96  bg-white  sm:my-auto  sm:w-9/12 mx-auto   sm:py-3  pb-52 w-full p-5 rounded-xl shadow-md border-t-4 border-t-blue-700 dark:border dark:border-slate-800 mb-5 dark:border-t-4 ">
+        <div class="min-h-96  bg-white  sm:my-auto  sm:w-9/12 mx-auto   sm:py-3  w-full p-5 rounded-xl shadow-md border-t-4 border-t-blue-700 dark:border dark:border-slate-800 mb-5 dark:border-t-4 ">
             <div class="text-justify"> 
                 <h2 class="text-2xl font-extrabold mb-4">Data Belum tersedia</h2> 
             </div> 
