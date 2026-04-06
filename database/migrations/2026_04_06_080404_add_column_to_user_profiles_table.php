2c8d8279a41f401d280803180b894e42d0bb08b7
usepgnwan <usepgnwan@gmail.com>
2026-04-06 15:40:50 +0700

    updat

---
+++ database/migrations/2026_04_06_080404_add_column_to_user_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            //
+            
+            $table->string('link_pengajar')->nullable();
+            $table->string('mapel_lainnya')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            //
+            
+            $table->dropColumn('link_pengajar');
+            $table->dropColumn('mapel_lainnya');
+        });
+    }
+};
